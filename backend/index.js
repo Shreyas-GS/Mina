486bfa94f7b51e8c9899fd31b9b6e0eb9cf61363
shashank <shashank974196@gmail.com>
2024-06-17 00:42:59 +0530

    code update

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,23 @@ mongoose.connect(
 app.get("/",(req,res)=>{
     res.send("Express app is running");
 })
+//Image Storage Engine 
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+      console.log(file);
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({storage: storage})
+app.use('/images', express.static('upload/images'));
+
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:4000/images/${req.file.filename}`
+    })
+})
 app.listen(port,(error)=>{
     if(!error){
         console.log('Server running on port',+port);
